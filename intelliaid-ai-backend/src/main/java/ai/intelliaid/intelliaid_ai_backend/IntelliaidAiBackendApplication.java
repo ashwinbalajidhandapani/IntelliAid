dc7903d469851bff2cbf4cae22cc2f6090a31661
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2024-07-24 18:41:35 -0400

    backend project - commit 1

---
+++ intelliaid-ai-backend/src/main/java/ai/intelliaid/intelliaid_ai_backend/IntelliaidAiBackendApplication.java
@@ -0,0 +1,13 @@
+package ai.intelliaid.intelliaid_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliaidAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliaidAiBackendApplication.class, args);
+	}
+
+}
---
+++ intelliaid-ai-backend/src/test/java/ai/intelliaid/intelliaid_ai_backend/IntelliaidAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.intelliaid.intelliaid_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliaidAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
